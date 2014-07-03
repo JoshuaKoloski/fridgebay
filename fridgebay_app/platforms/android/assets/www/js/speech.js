2fe7fd873b71afd68aeabe41b07af48e7aeced62
JoshuaKoloski <joshuakoloski@gmail.com>
2014-07-03 13:39:05 -0400

    div on top of html

--- fridgebay_app/platforms/android/assets/www/js/speech.js
+++
@@ -1,9 +0,0 @@
-navigator.speechrecognizer.recognize(successCallback, failCallback, 5, "Cordova Speech Recognizer Plugin");
-
-function successCallback(results){
-    console.log("Results: " + results);
-}
-
-function failCallback(error){
-    console.log("Error: " + error);
-}
