9a753a20b88d757eee0878b654e3344ea064bc4e
stevenjc <steven.j.colon@gmail.com>
2014-07-21 12:14:27 -0400

    Added phonegap

---
+++ fridgebay_app/www/js/controller.js
@@ -0,0 +1,81 @@
+
+var fridgeApp = (function($) {
+
+
+    // first create the model
+    var myList = new Information();
+    
+    var showView = function(selected) {
+      window.location.hash = '#' + selected;
+      $('.view').hide().filter('#' + selected + '-view').show();
+    };
+    
+    function showAlert() {
+        console.log("clicked");
+        alert("You have 2 new messages");
+    }
+    function showHelp() {
+        console.log("clicked");
+        alert("If you want to make a new post, click 'New Post'\nIf you want to buy an item, search through the list of items and sort by categories.");
+    }
+    function verifySubmission(){
+        showView("home");
+        alert("Your Message has been submitted");
+    }
+    function addItem(element) {
+        var element = document.getElementById("newItemName");
+
+        console.log("new item " + element.value);
+        myList.addElement({
+            action: element.value,
+            price: 0,
+            quantity: 0
+        });
+        element.value="";
+    }
+    
+
+    function refreshView(){
+        fridgeView.refreshView(myList);
+    }
+
+    function reloadModel(){
+        myList.loadModel();
+        refreshView();
+    }
+    
+    function initEventListeners(){
+        $(window).on('hashchange', function(event){
+          var view = (window.location.hash || '').replace(/^#/, '');
+          if ($('#' + view + '-view').length) {
+            showView(view);
+          }
+        });
+    }
+
+    function start() {
+        myList.loadModel();
+        showView("welcome");
+       
+   
+    }
+   
+
+   
+    // here is were we decide what is visible to the outside!
+    fridgeApp = {
+        start: start,
+        showAlert: showAlert,
+        showHelp: showHelp,
+        verifySubmission: verifySubmission,
+        refreshView: refreshView,
+        reloadModel: reloadModel,
+        showView: showView
+    }
+
+    return (fridgeApp);
+
+}(jQuery));
+
+
+
